19b8070445de54be69cbc7cae9b7717487afe148
Sckele <cheel0leech@gmail.com>
2025-06-24 14:00:33 +0300

    Reformatting; WIP: .docx parsing; NOTE: table fully prints in a test (with 'cargo run -- --nocapture'); TODO: putting data from table to a struct + wrapping it in a 'parse_docx' function

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
+use docx_rs::*;
 use rustydav::client;
 use serde::{Deserialize, Serialize};
-//Everything is going to user, so everything is a String
-struct Class /*Занятие*/ {
-    name: String,
-    teacher: String,
-    time: String,
-}
 struct GroupSchedule {
-    name: String,
-    classes: Vec<Class>,
+    group_name: String,
+    data: Vec<Vec<String>>, /*Schedule formatting is inconsistent, don't bother structuring*/
+    time: Vec<String>,
 }
 struct Credentials {
     url: String,
     user: String,
     password: String,
 }
+
+// NOTE:list_directories -> user interaction -> list_docx_files -> user interaction ->
+// download_docx
 // TODO:'()' as Err doen't look right, fix it(?)
 fn list_directories(credentials: &Credentials) -> Result<Vec<String>, ()> {
     todo!()
@@ -29,22 +28,57 @@ fn download_docx_file(
 ) -> Result<Vec<u8>, ()> {
     todo!()
 }
-fn parce_docx(docx_file: Vec<u8>) -> Result<Vec<GroupSchedule>, ()> {
+fn parse_docx(docx_file: Vec<u8>) -> Result<Vec<GroupSchedule>, ()> {
     todo!()
 }
-fn download_group_schedule(
-    credentials: &Credentials,
-    directory: &String,
-    filename: &String,
-    group_name: &String,
-) -> Result<GroupSchedule, ()> {
-    let docx = download_docx_file(credentials, directory, filename)?;
-    let groups = parce_docx(docx)?;
+fn get_group_by_name(groups: Vec<GroupSchedule>, group_name: &String) -> Result<GroupSchedule, ()> {
     for group in groups {
-        if group.name == *group_name {
+        if group.group_name == *group_name {
             return Ok(group);
         }
     }
     Err(())
 }
 fn main() {}
+#[test]
+fn parse_test() {
+    let buf = std::fs::read("schedule.docx").unwrap();
+    let docx = docx_rs::read_docx(&buf).unwrap();
+
+    // TODO:Reformat. Too nested
+    for child in docx.document.children {
+        match child{
+            docx_rs::DocumentChild::Table(table) /*full table!*/=> {
+                println!("Found a table!");
+                if table.rows.len() == 1{
+                    println!("\tIt's empty!");
+                    continue;
+                }
+                for row in table.rows{
+                    match row{
+                        TableChild::TableRow(row) => for cell in row.cells{
+                            print!("\t[");
+                            let mut cell_text = String::new();
+                            match cell{
+                                TableRowChild::TableCell(cell) => for (i, child) in cell.children.iter().enumerate(){
+                                    let cell_subtext = match child{
+                                        TableCellContent::Paragraph(text) => text.raw_text(),
+                                        _ => continue,
+                                    };
+                                    if cell_text.is_empty() {
+                                        cell_text = cell_subtext;
+                                    }else{
+                                        cell_text = format!("{}, {}", cell_text, cell_subtext);
+                                    }
+                                },
+                            };
+                            print!("{}]", cell_text);
+                        }
+                    }
+                    println!();
+                }
+            },
+            _ => println!("Found not a table!"),
+        }
+    }
+}
