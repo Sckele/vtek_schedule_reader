23572ed24edb3ed8695be7a9a81d5a78b0b9f4a5
Sckele <cheel0leech@gmail.com>
2025-06-25 07:46:56 +0300

    Structs reformatting; Test renamed to 'vec_from_docx_is_err_test', reformatted; Wrapped docx parcing in 'vec_from_docx()' in 'impl GroupSchedule'; Error return type turned from () to Box<dyn std::error::Error>; schedule.docx changed (previous was mostly empty); WIP: .docx parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use docx_rs::*;
-use rustydav::client;
-use serde::{Deserialize, Serialize};
+#[derive(Debug)]
 struct GroupSchedule {
     group_name: String,
-    data: Vec<Vec<String>>, /*Schedule formatting is inconsistent, don't bother structuring*/
-    time: Vec<String>,
+    details: Vec<GroupDetail>,
+}
+#[derive(Debug)]
+struct GroupDetail {
+    class: Vec<String>,
+    time: String,
 }
 struct Credentials {
     url: String,
@@ -12,6 +14,77 @@ struct Credentials {
     password: String,
 }
 
+impl GroupSchedule {
+    fn vec_from_docx(
+        docx: docx_rs::Docx,
+    ) -> Result<Vec<GroupSchedule>, Box<dyn std::error::Error>> {
+        let mut table: Option<Box<docx_rs::Table>> = None;
+
+        //searching through the docx to find docx_rs::Table
+        for c in docx.document.children {
+            if let docx_rs::DocumentChild::Table(t) = c {
+                const MINIMAL_ROWS: usize = 2; //idk who made these files to have 2 tables
+                if t.rows.len() <= MINIMAL_ROWS {
+                    continue;
+                }
+                table = Some(t);
+                break;
+            }
+        }
+
+        //converting docx_rs::Table to Vec<cargo::GroupSchedule>
+        let mut groups: Vec<GroupSchedule>;
+
+        if let Some(table) = table {
+            let row_count = table.rows.len();
+
+            //checks
+            if row_count % 2 == 1 {
+                return Err(format!("row count {row_count} incompatible!").into());
+            };
+            let mut reformatted_table: Vec<Vec<String>> = Vec::new();
+            for (i, row) in table.rows.iter().enumerate() {}
+
+            //convert docx_rs::Table to a more sane Vec<Vec<>>
+            //row
+            for row in table.rows {
+                let docx_rs::TableChild::TableRow(row) = row;
+                //cell
+                for cell in row.cells {
+                    print!("\t[");
+                    let mut cell_text = String::new();
+                    let docx_rs::TableRowChild::TableCell(cell) = cell;
+                    //cell data
+                    for (i, child) in cell.children.iter().enumerate() {
+                        let cell_subtext = match child {
+                            docx_rs::TableCellContent::Paragraph(text) => text.raw_text(),
+                            _ => continue,
+                        };
+                        if cell_text.is_empty() {
+                            cell_text = cell_subtext;
+                        } else {
+                            cell_text = format!("{}, {}", cell_text, cell_subtext);
+                        }
+                    }
+                    print!("{}]", cell_text);
+                }
+                println!();
+            }
+        }
+        Err("Group not formed!".into())
+    }
+    fn get_group_by_name(
+        groups: Vec<GroupSchedule>,
+        group_name: &String,
+    ) -> Result<GroupSchedule, ()> {
+        for group in groups {
+            if group.group_name == *group_name {
+                return Ok(group);
+            }
+        }
+        Err(())
+    }
+}
 // NOTE:list_directories -> user interaction -> list_docx_files -> user interaction ->
 // download_docx
 // TODO:'()' as Err doen't look right, fix it(?)
@@ -28,57 +101,13 @@ fn download_docx_file(
 ) -> Result<Vec<u8>, ()> {
     todo!()
 }
-fn parse_docx(docx_file: Vec<u8>) -> Result<Vec<GroupSchedule>, ()> {
-    todo!()
-}
-fn get_group_by_name(groups: Vec<GroupSchedule>, group_name: &String) -> Result<GroupSchedule, ()> {
-    for group in groups {
-        if group.group_name == *group_name {
-            return Ok(group);
-        }
-    }
-    Err(())
-}
 fn main() {}
 #[test]
-fn parse_test() {
+fn vec_from_docx_is_err_test() {
     let buf = std::fs::read("schedule.docx").unwrap();
     let docx = docx_rs::read_docx(&buf).unwrap();
 
-    // TODO:Reformat. Too nested
-    for child in docx.document.children {
-        match child{
-            docx_rs::DocumentChild::Table(table) /*full table!*/=> {
-                println!("Found a table!");
-                if table.rows.len() == 1{
-                    println!("\tIt's empty!");
-                    continue;
-                }
-                for row in table.rows{
-                    match row{
-                        TableChild::TableRow(row) => for cell in row.cells{
-                            print!("\t[");
-                            let mut cell_text = String::new();
-                            match cell{
-                                TableRowChild::TableCell(cell) => for (i, child) in cell.children.iter().enumerate(){
-                                    let cell_subtext = match child{
-                                        TableCellContent::Paragraph(text) => text.raw_text(),
-                                        _ => continue,
-                                    };
-                                    if cell_text.is_empty() {
-                                        cell_text = cell_subtext;
-                                    }else{
-                                        cell_text = format!("{}, {}", cell_text, cell_subtext);
-                                    }
-                                },
-                            };
-                            print!("{}]", cell_text);
-                        }
-                    }
-                    println!();
-                }
-            },
-            _ => println!("Found not a table!"),
-        }
-    }
+    let groups = GroupSchedule::vec_from_docx(docx);
+    println!("{:#?}", groups);
+    assert!(groups.is_ok());
 }
